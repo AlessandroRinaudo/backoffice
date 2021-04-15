141d50d055b265004da053f0019c1e08f84a9fe6
Quentin <quentin.joubert28@gmail.com>
2021-04-15 15:18:17 +0200

    add home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'detailsProduit', component: DetailsProductComponent },
   { path: 'home', component: HomeComponent }
 ];
