27769d36e6c221ea1dd5fab95b4a6588621099c2
Quentin <quentin.joubert28@gmail.com>
2021-04-21 16:58:58 +0200

    add trimestre filter

--- src/app/reporting/reporting.component.ts
+++ src/app/reporting/reporting.component.ts
@@ -21,27 +21,30 @@ export class ReportingComponent implements OnInit {
       label: 'Chiffres d\'affaires',
       data: this.dataCA,
       fill: false,
-      borderColor: 'rgb(75, 192, 192)',
+      borderColor: 'rgb(0, 0, 255)',
+      backgroundColor: 'rgb(0, 0, 0)',
       tension: 0.1
     },
       {
         label: 'Cout',
         data: this.dataCout,
         fill: false,
-        borderColor: 'rgb(192, 192, 192)',
+        borderColor: 'rgb(255, 0, 0)',
+        backgroundColor: 'rgb(0, 0, 0)',
         tension: 0.1
       },
     {
         label: 'Marge',
       data: this.dataMarge,
         fill: false,
-        borderColor: 'rgb(192, 192, 192)',
+        borderColor: 'rgb(0, 255, 0)',
+        backgroundColor: 'rgb(0, 0, 0)',
         tension: 0.1
       }]
   };
 
   transactions = null;
-  filtrage = [{ name: 'all' }, { name: 'année' }, { name: 'mois' }, { name: 'semaine' }, { name: 'jour' }]
+  filtrage = [{ name: 'all' }, { name: 'année' }, { name: 'trimestre' }, { name: 'mois' }, { name: 'semaine' }, { name: 'jour' }]
   chiffreAffaires = 0;
   marge = 0;
   impot = 0;
@@ -107,12 +110,14 @@ export class ReportingComponent implements OnInit {
     this.chiffreAffaires = 0
     let today = new Date();
     let todayYear = today.getFullYear();
+    let todayTrimestre = Math.ceil((today.getMonth() + 1) / 4)
     let todayMonth = today.toLocaleString('default', { month: 'long' })
     let todayMonthDay = today.getDate();
     for (let i = 0; i < this.transactions.length; i++) {
       let dateExist = false
       let date = new Date(this.transactions[i].created)
       let transacYear = date.getFullYear();
+      let transacTrimestre = Math.ceil((date.getMonth() + 1) / 4)
       let transacMonth = date.toLocaleString('default', { month: 'long' })
       let transacMonthDay = date.getDate();
       let transacWeekday = date.toLocaleString('default', { weekday: 'long' })
@@ -132,6 +137,24 @@ export class ReportingComponent implements OnInit {
               this.dataCout[this.dataCout.length] = 0
               this.dataCA[this.dataCA.length] = this.transactions[i].price;
               this.labels[this.labels.length] = transacMonth;
+              this.dataMarge[this.dataMarge.length] = this.transactions[i].price
+            }
+          }
+        }
+        else if (filtrage == "trimestre") {
+          if (transacYear == todayYear) {
+            this.chiffreAffaires = this.chiffreAffaires + this.transactions[i].price
+            for (let j = 0; j < this.labels.length; j++) {
+              if (this.labels[j] == transacTrimestre) {
+                this.dataCA[j] = this.dataCA[j] + this.transactions[i].price
+                this.dataMarge[j] = this.dataMarge[j] + this.transactions[i].price
+                dateExist = true;
+              }
+            }
+            if (dateExist == false) {
+              this.dataCout[this.dataCout.length] = 0
+              this.dataCA[this.dataCA.length] = this.transactions[i].price;
+              this.labels[this.labels.length] = transacTrimestre;
               this.dataMarge[this.dataMarge.length] = this.transactions[i].price
             }
           }
@@ -224,6 +247,23 @@ export class ReportingComponent implements OnInit {
             }
           }
         }
+        if (filtrage == "trimestre") {
+          if (transacYear == todayYear) {
+            for (let j = 0; j < this.labels.length; j++) {
+              if (this.labels[j] == transacTrimestre) {
+                this.dataCout[j] = this.dataCout[j] + this.transactions[i].price
+                this.dataMarge[j] = this.dataMarge[j] - this.transactions[i].price
+                dateExist = true;
+              }
+            }
+            if (dateExist == false) {
+              this.dataCA[this.dataCA.length] = 0
+              this.dataCout[this.dataCout.length] = this.transactions[i].price;
+              this.labels[this.labels.length] = transacTrimestre;
+              this.dataMarge[this.dataMarge.length] = (0 - this.transactions[i].price)
+            }
+          }
+        }
         else if (filtrage == "mois") {
           if (transacYear == todayYear && transacMonth == todayMonth) {
             for (let j = 0; j < this.labels.length; j++) {
@@ -292,6 +332,11 @@ export class ReportingComponent implements OnInit {
       }
     }
     this.initChart();
+  }
+
+  getWeek(d) {
+    var firstDay = new Date(d.getFullYear(), d.getMonth(), 1).getDay();
+    return Math.ceil((d.getDate() + (firstDay - 1)) / 7);
   }
 
   getMarge() {
